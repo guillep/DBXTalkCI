ef41f1cbc3bff2b5f1096b09c84d630da2bc83b0
GuillePolito <guillermopolito@gmail.com>
2014-05-09 15:53:12 +0200

    trying to make it work...

--- opendbx/build.sh
+++ opendbx/build.sh
@@ -7,7 +7,8 @@ mkdir results
 echo "Will install libraries in $WORKSPACE/results"
 cd OpenDBX
 
-LDFLAGS="-L/usr/lib" . ./configure --prefix="$WORKSPACE/results/" --disable-utils --with-backends="$backend"
+LDFLAGS="-L/usr/lib"
+. ./configure --prefix="$WORKSPACE/results/" --disable-utils --with-backends="$backend"
 
 make && make install
 
