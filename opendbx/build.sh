66c52c8aff4440536c2965238ebf9b8aa0fff0d9
GuillePolito <guillermopolito@gmail.com>
2014-05-09 15:36:50 +0200

    adding debug printfs

--- opendbx/build.sh
+++ opendbx/build.sh
@@ -4,6 +4,8 @@ rm -rf results
 mkdir results
 echo "Will install libraries in $WORKSPACE/results"
 cd OpenDBX
+
+echo ${CPPFLAGS}
 CPPFLAGS="${CPPFLAGS}" LDFLAGS="-L/usr/lib" ./configure --prefix="$WORKSPACE/results/" --disable-utils --with-backends="$backend"
 
 make && make install
