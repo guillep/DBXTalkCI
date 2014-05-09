8d6b85d20376afa9494a690bd4c970a6b3376156
GuillePolito <guillermopolito@gmail.com>
2014-05-09 15:32:43 +0200

    not cloning to current dir

--- opendbx/build.sh
+++ opendbx/build.sh
@@ -1,10 +1,12 @@
-git clone https://github.com/guillep/OpenDBX.git .
+git clone https://github.com/guillep/OpenDBX.git
 
 rm -rf results
 mkdir results
 echo "Will install libraries in $WORKSPACE/results"
+cd OpenDBX
 LDFLAGS="-L/usr/lib" ./configure --prefix="$WORKSPACE/results/" --disable-utils --with-backends="$backend"
 
 make && make install
 
+cd ..
 mv results/lib/opendbx/* results/lib
