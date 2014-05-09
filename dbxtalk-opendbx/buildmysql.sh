62cc19713093772234dc1eeff69749bd33493f8d
GuillePolito <guillermopolito@gmail.com>
2014-05-09 17:08:37 +0200

    added symlinks to libraries

--- dbxtalk-opendbx/buildmysql.sh
+++ dbxtalk-opendbx/buildmysql.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 export BACKEND="mysql"
+ln -s /usr/lib/lib${BACKEND}client.so lib${BACKEND}client.so
 ./scripts/dbxtalk-opendbx/build.sh
--- dbxtalk-opendbx/buildoracle.sh
+++ dbxtalk-opendbx/buildoracle.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 export BACKEND="oracle"
+ln -s /usr/lib/libpq.so libpq.so
 ./scripts/dbxtalk-opendbx/build.sh
